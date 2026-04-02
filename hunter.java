288da6fa93bf4ddf2d04ede65c6bace9152ce7ea
Sgowtham56 <gmgowtham514@gmail.com>
2026-04-02 10:27:21 +0530

    Java

---
+++ hunter.java
@@ -0,0 +1,14 @@
+class hunter{
+    public static void main(String args[])
+    {
+    int a = 100;
+    int b = 50;
+    int c = 60;
+    int d = a+b+c;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(a+b+c);
+
+    }
+}
