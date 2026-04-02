26ef72951fba650adf714bd2403adc949c98f054
Sgowtham56 <gmgowtham514@gmail.com>
2026-04-02 12:12:52 +0530

    battle

---
+++ battle.java
@@ -0,0 +1,12 @@
+class battle{
+    public static void main(String args[])
+    {
+        int man power = 10000;
+        int fly power = 4500;
+        int gun = 1000000;
+        int gun powder value = 100;
+        int vechical power = 6000;
+        int nuclear weppon = 1000;
+        System.out,println("Battle"+power+gun+weppon);
+    }
+}
