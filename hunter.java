1755733107990263ccf22b4bb8ba522756988fcd
Sgowtham56 <gmgowtham514@gmail.com>
2026-04-02 10:29:47 +0530

    Update hunter.java

--- hunter.java
+++ hunter.java
@@ -5,10 +5,12 @@ class hunter{
     int b = 50;
     int c = 60;
     int d = a+b+c;
+        int e = d;
     System.out.println(a);
     System.out.println(b);
     System.out.println(c);
     System.out.println(a+b+c);
+         System.out.println(d);
 
     }
 }
